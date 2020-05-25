77d023630279a3dfbf30c82da7fd2b357e8e9b5e
nafeeur10 <nafeeur.kuet.cse6@gmail.com>
2020-05-25 11:35:13 +0600

    Codeforces Template

--- Codeforces/contest.cpp
+++ Codeforces/contest.cpp
@@ -1,1 +1,21 @@
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define LL long long
+
+int main()
+{
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+    #endif
+
+    int t;
+    cin>>t;
+    while(t--) {
+        
+    }
+    return 0;
+}

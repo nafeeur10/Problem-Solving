f9229ce88c681413b11a78b7ba6a5c0e32d51268
nafeeur10 <nafeeur.kuet.cse6@gmail.com>
2021-02-18 12:35:00 +0600

    Add files via upload

--- LeetCode/1480. Running Sum of 1d Array/main.cpp
+++ LeetCode/1480. Running Sum of 1d Array/main.cpp
@@ -1,9 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+class Solution {
+    public:
+        vector<int> runningSum(vector<int>& nums) {
+            int sum = 0;
+            vector<int>res;
+            for(int i = 0; i < nums.size(); i++)
+            {
+                sum+=nums[i];
+                res.push_back(sum);
+            }
+            return res;
+        }
+};
+
 int main()
 {
-    cout << "Alhamdulillah" << endl;
+    Solution solution;
+    vector<int>nums{1, 2, 3, 4};
+    vector<int>result = solution.runningSum(nums);
+    for(int i = 0; i < result.size(); i++) {
+        cout<<result[i]<<endl;
+    }
     return 0;
 }

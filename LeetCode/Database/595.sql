3a9c9d20f89aeafa9e830d2d5232f774c7eeec95
Nafeeur Rahman <nafeeur.kuet.cse6@gmail.com>
2021-10-10 14:33:45 +0600

    595

---
+++ LeetCode/Database/595.sql
@@ -0,0 +1,3 @@
+# Problem Link: https://leetcode.com/problems/big-countries/
+# Write your MySQL query statement below
+SELECT name, population, area FROM World WHERE area > 3000000 OR population > 25000000;

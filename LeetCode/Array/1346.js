4910deaa98aa043eb787eb381c4c6ad4286768a9
Nafeeur Rahman <nafeeur.kuet.cse6@gmail.com>
2021-12-23 12:59:33 +0600

    1346

---
+++ LeetCode/Array/1346.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+ var checkIfExist = function(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr.length; j++) {
+            if(arr[i] == 2*arr[j] && i != j) {
+                return true
+            }
+        }
+    }
+    return false
+};

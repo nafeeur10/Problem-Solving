2d5abfb82d441f306005395cdc12e865a39f2e54
Nafeeur Rahman <nafeeur.kuet.cse6@gmail.com>
2023-12-18 12:24:09 +0600

    Time: 78 ms (40.65%) | Memory: 42.7 MB (85.02%) - LeetSync

--- LeetCode/242-valid-anagram/valid-anagram.js
+++ LeetCode/242-valid-anagram/valid-anagram.js
@@ -3,28 +3,25 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-    let mapS = new Map();
-    let mapT = new Map();
+var isAnagram = function (s, t) {
+  let mapS = new Map();
+  let mapT = new Map();
 
-    for(let i = 0; i < s.length; i++) {
-        mapS.set(s[i], mapS.get(s[i]) + 1 || 1);
+  if (s.length !== t.length) return false;
+
+  for (let i = 0; i < s.length; i++) {
+    mapS.set(s[i], mapS.get(s[i]) + 1 || 1);
+    mapT.set(t[i], mapT.get(t[i]) + 1 || 1);
+  }
+
+  for (const [key, value] of mapS) {
+    if (mapT.get(key) == value) {
+    } else {
+      return false;
     }
+  }
 
-    for(let i = 0; i < t.length; i++) {
-        mapT.set(t[i], mapT.get(t[i]) + 1 || 1);
-    }
-
-    if(mapS.size !== mapT.size) return false
-
-    for (const [key, value] of mapS) {
-        if(mapT.get(key) == value ) {
-
-        } else {
-            return false;
-        }
-        //console.log(`${key} = ${value}`);
-      }
-
-    return true;
+  return true;
 };
+
+console.log(isAnagram("a", "ab"));

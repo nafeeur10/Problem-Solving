98897a84d3baeb2e75950eece7734bfa2251067c
Nafeeur Rahman <nafeeur.kuet.cse6@gmail.com>
2023-12-28 17:47:40 +0600

    Time: 500 ms (5.24%) | Memory: 50.7 MB (5.09%) - LeetSync

--- LeetCode/1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.js
+++ LeetCode/1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.js
@@ -6,28 +6,21 @@ var minOperations = function (s) {
   let minStart = 0;
   let minEnd = 0;
 
-  let sFakeWithStart0 = "";
-  let sFakeWithStart1 = "";
+  let lengthS = s.length;
 
-  for (let i = 0; i < s.length; i++) {
-    if (i % 2 == 0) {
-      sFakeWithStart0 += "0";
-      sFakeWithStart1 += "1";
-    } else {
-      sFakeWithStart0 += "1";
-      sFakeWithStart1 += "0";
+  let j = 1;
+  for (let i = 0; i < lengthS; i += 2) {
+    console.log(s[i] - '0' + s[j] - '0');
+    if (s[i] - "0" != i % 2 && i < lengthS) {
+      minStart++;
     }
+    if (s[j] - "0" != j % 2 && j < lengthS) {
+      minStart++;
+    }
+    j += 2;
   }
 
-  for (let i = 0; i < s.length; i++) {
-    if(s[i] !== sFakeWithStart0[i]) {
-        minStart++;
-    }
+  console.log(minStart);
 
-    if(s[i] !== sFakeWithStart1[i]) {
-        minEnd++;
-    }
-  }
-
-  return Math.min(minStart, minEnd);
+  return Math.min(minStart, lengthS - minStart);
 };
